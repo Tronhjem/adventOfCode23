8a692f1a7a27242ffb304f7ac97db4e1b8bf9afc
Christian Tronhjem <christian.tronhjem@ubisoft.com>
2023-12-01 10:56:24 +0100

    rust

---
+++ adventOfCodeRust/src/main.rs
@@ -0,0 +1,15 @@
+use std::fs::read;
+
+fn read_file_vec(filepath: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let data = read(filepath)?;
+    Ok(data)
+}
+
+fn main() 
+{
+    let result = read_file_vec("../../../1.txt");
+    for line in result 
+    {
+        println!("{}", line?);
+    }
+}
